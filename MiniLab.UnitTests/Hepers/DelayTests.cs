32d3bf330f32aaa76d84a4497448787407cb7bd6
Bhavnit Singh Bajwa <bajwabsingh@yahoo.com>
2014-02-17 10:59:48 +0000

    - Implemented Helpers.IDelay     :: Used to provide "Delay" by putting the current executing thread to sleep.     - Implemented 'Delay'     - Added SystemWrapper and SystemInterface in lib.

---
+++ MiniLab.UnitTests/Hepers/DelayTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MiniLab.Helpers;
+
+using SystemInterface.Threading;
+
+using NUnit.Framework;
+using Moq;
+
+namespace MiniLab.Tests.Hepers.Delays
+{
+    class basic_delay_test_fixture
+    {
+        protected IDelay _delay;
+        protected Mock<IThread> _mockThread;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockThread = new Mock<IThread>(MockBehavior.Strict);
+            _delay = new Delay(_mockThread.Object);
+        }
+
+        [TearDown]
+        public void VerifyMocks()
+        {
+            _mockThread.VerifyAll();
+        }
+    }
+
+    [TestFixture]
+    class when_requesting_delay_in_milliseconds : basic_delay_test_fixture
+    {
+        [Test]
+        public void _01_SHOULD_put_the_thread_to_sleep_for_10_ms_WHEN_10_ms_delay_has_been_requested()
+        {
+            _mockThread.Setup(thread => thread.Sleep(10));
+
+            _delay.InMilliseconds(10);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException), ExpectedMessage = "milliSeconds cannot be zero or negative value.")]
+        public void _02_SHOULD_throw_InvalidArgumentException_WHEN_passed_milliSeconds_is_less_than_or_equal_to_zero()
+        {
+            _delay.InMilliseconds(0);
+        }
+    }
+
+    [TestFixture]
+    class when_requesting_delay_in_seconds : basic_delay_test_fixture
+    {
+        [Test]
+        public void _01_SHOULD_put_the_thread_to_sleep_for_10_seconds_WHEN_10_second_delay_has_been_requested()
+        {
+            _mockThread.Setup(thread => thread.Sleep(10 * 1000));
+
+            _delay.InSeconds(10);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException), ExpectedMessage = "seconds cannot be zero or negative value.")]
+        public void _02_SHOULD_throw_InvalidArgumentException_WHEN_passed_seconds_is_less_than_or_equal_to_zero()
+        {
+            _delay.InSeconds(0);
+        }
+    }
+}
---
+++ MiniLab/Helpers/Delay.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SystemInterface.Threading;
+
+namespace MiniLab.Helpers
+{
+    public interface IDelay
+    {
+        void InSeconds(int seconds);
+        void InMilliseconds(int milliSeconds);
+    }
+
+    /// <summary>
+    /// Provides Delay related services.
+    /// <remarks>
+    /// Delay is provided by putting the current executing thread to sleep. 
+    /// Thereby actual delay provided is equal to "atleast" the requested amount of delay.
+    /// Under heavy CPU load, actual delay might be more than requested amount.
+    /// </remarks>
+    /// </summary>
+    public class Delay : IDelay
+    {
+        IThread _thread;
+
+        public Delay(IThread thread)
+        {
+            _thread = thread;
+        }
+
+        /// <summary>
+        /// Causes the current thread to sleep for provided milliseconds.
+        /// </summary>
+        /// <param name="milliseconds"></param>
+        public void InMilliseconds(int milliseconds)
+        {
+            if (milliseconds <= 0)
+                throw new ArgumentException("milliSeconds cannot be zero or negative value.");
+
+            _thread.Sleep(milliseconds);
+        }
+
+        /// <summary>
+        /// Causes the current thread to sleep for provided seconds.
+        /// </summary>
+        /// <param name="seconds"></param>
+        public void InSeconds(int seconds)
+        {
+            if (seconds <= 0)
+                throw new ArgumentException("seconds cannot be zero or negative value.");
+
+            _thread.Sleep(seconds * 1000);
+        }
+    }
+}
