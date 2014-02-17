f0073421625a173b65d0362835de2807a0ad9004
Bhavnit Singh Bajwa <bajwabsingh@yahoo.com>
2014-02-17 15:01:04 +0000

    - Implemented MiniLab.Helpers.Gestures     :: Provides functionality to enlist a set of actions and 'play' (act upon each) when directed.     - Added:         - Gesture.Actions (a list of actions contained within the gesture)         - Gesture.Play (Plays the set of actions in exactly the same sequence in which they were added).

---
+++ MiniLab.UnitTests/Helpers/GestureTests.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MiniLab.Helpers;
+
+using Moq;
+using NUnit.Framework;
+
+namespace MiniLab.Tests.Helpers.Gestures
+{
+    public class gesture_test_fixture_base
+    {
+        protected Gesture _gesture;
+
+        [SetUp]
+        public void Setup()
+        {
+            _gesture = new Gesture();
+        }
+    }
+
+    [TestFixture]
+    public class when_creating_a_new_gesture : gesture_test_fixture_base
+    {
+        [Test]
+        public void _01_SHOULD_create_an_instance_with_no_actions()
+        {
+            Assert.That(_gesture.Actions, Is.Empty);
+        }
+    }
+
+    [TestFixture]
+    public class when_adding_actions_into_a_gesture : gesture_test_fixture_base
+    {
+        private void DummyAction1() { }
+        private void DummyAction2() { }
+        
+        [Test]
+        public void _01_SHOULD_add_the_passed_item_into_gesture()
+        {
+            _gesture.Actions.Add(DummyAction1);
+            _gesture.Actions.Add(DummyAction2);
+
+            Assert.That(_gesture.Actions.Count, Is.EqualTo(2));
+            Assert.That(_gesture.Actions[0], Is.EqualTo((Action)DummyAction1));
+            Assert.That(_gesture.Actions[1], Is.EqualTo((Action)DummyAction2));
+        }
+
+        [Test]
+        public void _02_AddActionsFromGesture_SHOULD_add_all_actions_from_the_passed_gesture()
+        {
+            Gesture dummyGesture = new Gesture();
+            dummyGesture.Actions.Add(DummyAction1);
+            dummyGesture.Actions.Add(DummyAction2);
+
+            _gesture.AddActionsFromGesture(dummyGesture);
+
+            Assert.That(_gesture.Actions, Is.EqualTo(dummyGesture.Actions));
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void _03_AddActionsFromGesture_SHOULD_throw_ArgumentNullException_WHEN_gesture_is_passed_null()
+        {
+            _gesture.AddActionsFromGesture(null);
+        }
+    }
+
+    [TestFixture]
+    public class when_playing_a_gesture : gesture_test_fixture_base
+    {
+        [Test]
+        public void _01_SHOULD_play_each_action_in_the_right_sequence()
+        {
+            Stack<int> recordedSequence = new Stack<int>();
+            Stack<int> expectedSequence = new Stack<int>();
+            expectedSequence.Push(1);
+            expectedSequence.Push(2);
+            expectedSequence.Push(3);
+            _gesture.Actions.Add(() => recordedSequence.Push(1));
+            _gesture.Actions.Add(() => recordedSequence.Push(2));
+            _gesture.Actions.Add(() => recordedSequence.Push(3));
+
+
+            _gesture.Play();
+
+
+            Assert.That(recordedSequence, Is.EqualTo(expectedSequence));
+        }
+    }
+}
---
+++ MiniLab/Helpers/Gesture.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniLab.Helpers
+{
+    /// <summary>
+    /// Provides functionality to enlist a set of actions and 'play' (act upon each) when directed.
+    /// </summary>
+    public class Gesture
+    {
+        /// <summary>
+        /// Gets the list of Actions that is contained within the Gesture instance.
+        /// </summary>
+        public List<Action> Actions { get; set; }
+
+        /// <summary>
+        /// Creates a new Gesture instance.
+        /// </summary>
+        public Gesture()
+        {
+            Actions = new List<Action>();
+        }
+
+        /// <summary>
+        /// Plays the set of actions in exactly the same sequence in which they were added.
+        /// </summary>
+        public void Play()
+        {
+            foreach (Action action in Actions)
+                action();
+        }
+
+        /// <summary>
+        /// Adds actions from another gesture instance.
+        /// </summary>
+        /// <param name="gesture">The gesture instance from which to borrow actions from.</param>
+        public void AddActionsFromGesture(Gesture gesture)
+        {
+            if (gesture == null)
+                throw new ArgumentNullException("gesture");
+
+            Actions.AddRange(gesture.Actions);
+        }
+    }
+}
